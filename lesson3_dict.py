ddecce5b21d984be05d790494352f564ae426c48
pythonemus <n.dambrosio67@gmail.com>
2022-11-16 03:58:39 +0000

    add dict lesson

---
+++ lesson3_dict.py
@@ -0,0 +1,16 @@
+'''
+Dictionary list are used to story data in a key:value paring.
+These list do not allow duplicates.
+As of Python version 3.7, dictionaries are ordered. 
+In Python 3.6 and earlier, dictionaries are unordered.
+'''
+
+dict1 = {
+    "make":"chevy",
+    "model":"cavelier",
+    "year": 1990
+}
+print(dict1)
+
+# You can reference an item pairing by key name
+print(dict1["year"]) # This will print the value of "year"
