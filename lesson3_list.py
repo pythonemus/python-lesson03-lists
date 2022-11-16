faa3e9cba5d508b68693ca696072dee8508c2673
pythonemus <n.dambrosio67@gmail.com>
2022-11-16 03:40:40 +0000

    lesson list update

--- lesson3_list.py
+++ lesson3_list.py
@@ -2,3 +2,11 @@
 # The list is made up of single values
 
 list1 = ['red', 'blue', 'green']
+print(list1)
+
+# Python will natively track the items in list with index identifiers
+# These index identifiers are numeric starting from zero (0)
+# You can reference an item in the list by index ID number
+
+print(list1[2])
+# This code should print 'green' from the list
